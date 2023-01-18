afb99e6d4878a4422c4a6d53fc267163bcd8e911
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2023-01-18 10:24:59 -0300

    Implementando backend da aplicação.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,26 +1,12 @@
 import FatiFy from 'fastify'
-import {PrismaClient} from '@prisma/client'
 import cors from '@fastify/cors'
-
+import { appRouter } from './routes'
 
 const app = FatiFy()
-const prisma = new PrismaClient()
-
-/**
- * Metodos HTTP: Get, Post, Put, Patch, Delete
- */
 
 app.register(cors)
-app.get('/', async () => {
-  const habits = await prisma.habit.findMany({
-    where:{
-      title:{
-        startsWith:'Beber'
-      }
-    }
-  })
-  return habits
-})
+app.register(appRouter)
+
 
 app.listen({
   port: 3333
