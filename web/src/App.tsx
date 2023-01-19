54498b0e4345200c17ec5c96a43c4db0a8b494ff
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2023-01-19 08:04:19 -0300

    Adicionando  mais informaçãoes ao front-end.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,14 @@
-import { Habit } from "./components/Habit";
+import { Header } from './components/Header'
+import { SummaryTable } from './components/Summarytable'
 import './styles/global.css'
 
 export function App() {
-
   return (
-    <>
-      <Habit desempenho={3}/>
-      <Habit desempenho={10}/>
-      <Habit desempenho={20}/>
-      <Habit desempenho={30}/>
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
   )
 }
-
---
+++ web/src/components/HabitDay/index.tsx
@@ -0,0 +1,8 @@
+/* interface HabitProps {
+  desempenho: number
+} */
+export function HabitDay(/* { desempenho }: HabitProps */) {
+  return (
+    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+  )
+}
